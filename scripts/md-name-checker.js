88e888d0854569431348e7b2a0b980691dd1ecdc
Sorin <scurescu@suse.com>
2019-10-29 17:34:48 +0100

    Added task grunt checkForDuplicates

--- scripts/md-name-checker.js
+++ scripts/md-name-checker.js
@@ -2,23 +2,27 @@ const fs = require('fs-extra')
 const { lstatSync, readdirSync } = require('fs')
 const { join } = require('path')
 
-
-/* RegEx to match svg size */
-const regEx = /(.*)24px.svg/gm
-
 /* Filter out directories */
 const isDirectory = source => lstatSync(source).isDirectory()
 const getDirectories = async source => readdirSync(source).map(name => join(source, name)).filter(isDirectory)
 
 const readFiles = async dir => {
+  /* RegEx to match svg size */
+  const regEx = /(.*)24px.svg/gm
+  
   try {
-    return  await fs.readdirSync(dir, (err, filenames) => filenames.filter(icon => icon.match(regEx)))
-  } catch (error) {
-    return
+    const icons = await fs.readdirSync(dir, (err, filenames) => filenames)
+
+    if(dir.includes('node_modules')) {
+      return icons.filter(item => item.match(regEx)).map(ele => ele.replace('ic_', '').replace('_24px', ''))
+    } else {
+      return icons
+    }
+
+  } catch (error) {    return
   }
 }
 
-// const filterd = matchFiles.map(ele => ele.replace('ic_', '').replace('_24px', '').replace('.svg', ''))
 
 const fetchIcons = async mainDir => {
  try {
@@ -43,25 +47,19 @@ const checkIcons = async params => {
     const { mdRepo, eosRepo } = params
 
     /* Get the array for md and eos icons */
-    const mdIcons = await fetchIcons(mdRepo)
-    const eosIcons = await readFiles(eosRepo)
-    
+    const mdIcons = await fetchIcons(mdRepo) // ?
+    const eosIcons = await readFiles(eosRepo) // ?
 
     const checkForMatchingIcon = mdIcons.filter(element => {
-      return eosIcons.includes(element )
+      return eosIcons.includes(element)
     });
 
-    return checkForMatchingIcon.length >= 1 ? `ERROR!: Duplicate name: ${checkForMatchingIcon}` : 'No duplicates'
+    return checkForMatchingIcon.length >= 1 ? ` ⚠️  Warning!: Duplicate name for: ${checkForMatchingIcon}` : 'No duplicates'
 
   } catch (error) {
     console.log('ERROR: checkIcons() => : ', error);
   }
 }
-
-const mdRepo = './node_modules/material-design-icons'
-const eosRepo = './svg'
-
-checkIcons({ mdRepo, eosRepo }).then(d => console.log(d))
 
 module.exports = {
   checkIcons
