f5e7dd23b023f0556780185258da4b41fe210c10
Sorin <scurescu@suse.com>
2019-10-30 12:06:59 +0100

    Run default tasks

--- scripts/md-name-checker.js
+++ scripts/md-name-checker.js
@@ -77,7 +77,9 @@ const checkIcons = async params => {
     /**
      * We return a warning or success message bases on the result
      */
-    return checkForMatchingIcon.length >= 1 ? { error: true, message: `⚠️  Duplicate name for: ${checkForMatchingIcon}` } : { error: false, message: `✅  No duplicates`}
+    return checkForMatchingIcon.length >= 1
+      ? { error: true, message: `⚠️  Duplicate name for: ${checkForMatchingIcon}` }
+      : { error: false, message: `✅  No duplicates`}
 
   } catch (error) {
     console.log('ERROR: checkIcons() => : ', error);
