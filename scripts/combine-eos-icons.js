464ff749d283615cf4f847a4ace8b2208fa4fce0
scurescu <scurescu@suse.com>
2019-12-10 10:06:18 +0100

    Refactor and 3 news models

--- scripts/combine-eos-icons.js
+++ scripts/combine-eos-icons.js
@@ -1,23 +1,27 @@
 const fs = require('fs-extra')
 
 const combineIconsModels = async params => {
+  /* It takes a targetDir where the files.json are and the destination file. */
   const { targetDir, destDir } = params
 
+  /* Data holder */
   const data = []
 
-  return fs.readdir(targetDir, (err, files) => {
-    if (err) console.log(err)
-    return new Promise((resolve, reject) => {
-      if (err) reject(err)
-      files.forEach(file => {
-        const obj = JSON.parse(fs.readFileSync(`${targetDir}${file}`, 'utf8'))
-        data.push(obj)
-      })
-      resolve(data)
-    }).then(data => {
-      fs.writeFileSync(destDir, JSON.stringify(data, null, 2))
+  try {
+    const files = await fs.readdirSync(targetDir, (err, files) => {
+      if(err) console.log(err)
+
+      return files
     })
-  })
+
+    files.map(file => {
+      data.push(JSON.parse(fs.readFileSync(`${targetDir}${file}`, 'utf8')))
+    })
+
+    return fs.writeFileSync(destDir, JSON.stringify(data, null, 2))
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = {
