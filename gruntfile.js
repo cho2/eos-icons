d71221267593e6cc9372348ee153017cb5d963e9
bisht-richa <richa.bisht@suse.com>
2020-03-02 17:13:10 +0100

    Removed extra lines

--- gruntfile.js
+++ gruntfile.js
@@ -170,15 +170,7 @@ module.exports = function (grunt) {
       .then(done)
   })
 
-  /* Eos MD Icons Differences */
-  // grunt.registerTask('eosMdIconsDifferencesLog', async function () {
-
-  //   const done = this.async()
-
-  //   return eosMdIconsDifferences({targetDirMd: './svg/extended' })
-  //     .then(done)
-  // })
-
+/* compare MD icons in our repo and MD officical website */
   grunt.registerTask('eosMdIconsDifferencesLog', async function () {
     const done = this.async()
     await downlaodFile().then( () => {
