a6804194a3f7e3eb8153db591384b8d0d8ff6080
bisht-richa <richa.bisht@suse.com>
2021-02-16 16:00:00 +0100

    Fixed MD download duplicate icon issue

--- scripts/download-svg.js
+++ scripts/download-svg.js
@@ -73,10 +73,6 @@ const downloadMDFile = async (mdIconList) => {
     )
 
     if (svgCollection.includes(mdIcon)) {
-      console.log(
-        `The SVG/ folder already has an icon with ${mdIcon} name. Please review the design before confirming.`
-      )
-
       await duplicateMDIcon().then(async (response) => {
         if (response.answer === 'Yes') {
           addDuplicateName(mdIcon)
