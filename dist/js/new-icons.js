5f0eeddf689f958224a85e52699b4855cf2bf310
bisht-richa <richa.bisht@suse.com>
2021-03-24 11:44:10 +0100

    Fix: Created tabs for new icons MD vs EOS

--- dist/js/new-icons.js
+++ dist/js/new-icons.js
@@ -1,22 +1,47 @@
 ;(function () {
-  latestIcon('date')
+  latestIcon('date', 'eos-icons')
   // Switch normal and outline icons
   document.getElementById('switch-outlined-icon').onclick = function (e) {
     e.preventDefault()
     const link = document.getElementById('css-link')
-
+    const selectedTabName = tabSelected()
     if (link.getAttribute('href') === 'css/eos-icons.css') {
       link.setAttribute('href', 'css/outlined/eos-icons-outlined.css')
       document.getElementById('switch-checkbox').checked = true
-      latestIcon('dateOutlined')
+      latestIcon('dateOutlined', selectedTabName)
     } else {
       link.setAttribute('href', 'css/eos-icons.css')
       document.getElementById('switch-checkbox').checked = false
-      latestIcon('date')
+      latestIcon('date', selectedTabName)
+      
     }
   }
 
-  function latestIcon(isOutlined) {
+  function tabSelected() {
+    const tabSelected = document.getElementsByClassName('selected')[0].getAttributeNode('data-name').value
+    return tabSelected
+  }
+
+  // Swithch between EOS and MD tabs
+  document.getElementById('tab').onclick = function (e) {
+    e.preventDefault()
+    const tabName = document.getElementsByClassName('icons-tab')
+    const targetName = e.target.getAttribute('data-name') || e.target.parentNode.getAttribute('data-name');
+    for(var i = 0; i < tabName.length; i++) {
+      tabName[i].className  = 'icons-tab'
+    }
+    e.target.className  = 'icons-tab selected'
+
+    // Check for Outlined and filled version
+    const outlineCheck = document.getElementById('switch-checkbox').checked
+    if (outlineCheck) {
+      latestIcon('dateOutlined', targetName)
+    } else {
+      latestIcon('date', targetName)
+    }
+  }
+
+  function latestIcon(isOutlined, targetName) {
     // Empty latest section
     document
       .querySelectorAll('.latest-icons .icons__item')
@@ -51,8 +76,16 @@
           }
 
           const target = document.querySelector('.latest-icons')
-          // Appends each icon to the preview wrap
-          newIconsList.forEach((ele) => {
+          // Filter EOS and MD icons
+         let newIcon
+          if (targetName === 'eos-icons') {
+            newIcon = newIconsList.filter(ele => ele.tags.includes('eos'))
+          } else {
+            newIcon = newIconsList.filter(ele => !ele.tags.includes('eos'))
+          }
+
+          // Appends each icon to the preview wrap 
+          newIcon.forEach((ele) => {
             const div = document.createElement('div')
 
             div.classList.add('icons__item')
