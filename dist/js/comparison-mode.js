c556b2dd4411ca3ac5ad2c3836c893f6cb333925
Jesus Herman-Marina <jherman@suse.com>
2021-04-22 15:25:41 +0200

    compare mode functionality

--- dist/js/comparison-mode.js
+++ dist/js/comparison-mode.js
@@ -1,11 +1,40 @@
 ;(function () {
 
   window
-    fetch('../js/glyph-list.json')
-          .then(res => res.json())
-          .then(data => {
-            console.log(data)
+    .fetch('../js/glyph-list.json')
+          .then(response => response.json())
+          .then((data) => {
+            try {
+
+              const target = document.querySelector('#icons')
+              // Filter EOS and MD icons
+              let ligatures = data.glyphs
+              console.log(ligatures);
+
+              // Appends each icon to the preview wrap
+              ligatures.forEach((glyph) => {
+                const div = document.createElement('div')
+
+                div.classList.add('icons__item')
+                div.setAttribute('name', glyph)
+                div.innerHTML = `
+                      <i class="eos-icons">
+                        ${glyph}
+                      </i>
+                      <i class="eos-icons-outlined">
+                        ${glyph}
+                      </i>
+                      <br>
+                      ${glyph}`
+                target.append(div)
+              })
+
+
+            } catch (error) {
+              console.error(error);
+            }
           })
-          .catch(err => console.error(err));
 
 })()
+
+
