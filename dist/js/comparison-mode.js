7f82fad9b5d59b916ba4e7d55157115df11e3097
Cynthia Sanchez <csanchez@suse.de>
2021-04-23 13:22:23 +0200

    refactor

--- dist/js/comparison-mode.js
+++ dist/js/comparison-mode.js
@@ -2,37 +2,29 @@
   window
     .fetch('../js/glyph-list.json')
     .then((response) => {
-      if (!response.ok) {
-        throw new Error('Run "npm run serve" to see this page correctly')
-      }
       return response.json()
     })
     .then((data) => {
-      try {
-        const target = document.querySelector('#icons')
-        // Filter EOS and MD icons
-        const ligatures = data.glyphs
+      const target = document.querySelector('#icons')
+      // Filter EOS and MD icons
+      const ligatures = data.glyphs
+      // Appends each icon to the preview wrap
+      ligatures.forEach((glyph) => {
+        const div = document.createElement('div')
 
-        // Appends each icon to the preview wrap
-        ligatures.forEach((glyph) => {
-          const div = document.createElement('div')
-
-          div.classList.add('icons__item')
-          div.setAttribute('name', glyph)
-          div.innerHTML = `
-                      <i class="eos-icons">
-                        ${glyph}
-                      </i>
-                      <i class="eos-icons-outlined">
-                        ${glyph}
-                      </i>
-                      <br>
-                      ${glyph}`
-          target.append(div)
-        })
-      } catch (error) {
-        console.error(error)
-      }
+        div.classList.add('icons__item')
+        div.setAttribute('name', glyph)
+        div.innerHTML = `
+                    <i class="eos-icons">
+                      ${glyph}
+                    </i>
+                    <i class="eos-icons-outlined">
+                      ${glyph}
+                    </i>
+                    <br>
+                    ${glyph}`
+        target.append(div)
+      })
     })
     .catch((error) => {
       const errorDiv = document.querySelector('.CORS-error')
--- scripts/demos/new-icons.js
+++ scripts/demos/new-icons.js
@@ -61,59 +61,58 @@
       .fetch('https://gitlab.com/api/v4/projects/4600360/repository/tags')
       .then((response) => response.json())
       .then((tags) => {
-        try {
-          const tagRelease = new Date(tags[0].commit.committed_date)
+        const tagRelease = new Date(tags[0].commit.committed_date)
 
-          // Filter out the icons that are newer than the latest release tag.
-          // eslint-disable-next-line no-undef
-          const newIconsList = eosIcons.filter((ele) => {
-            if (!ele[isOutlined]) return
-            const date = ele[isOutlined].split('/')
-            const itemDate = new Date(
-              date[2],
-              date[1],
-              date[0]
-            ).toLocaleDateString()
+        // Filter out the icons that are newer than the latest release tag.
+        // eslint-disable-next-line no-undef
+        const newIconsList = eosIcons.filter((ele) => {
+          if (!ele[isOutlined]) return
+          const date = ele[isOutlined].split('/')
+          const itemDate = new Date(
+            date[2],
+            date[1],
+            date[0]
+          ).toLocaleDateString()
 
-            if (itemDate < tagRelease.toLocaleDateString()) return ele
-          })
+          if (itemDate < tagRelease.toLocaleDateString()) return ele
+        })
 
-          // Removes the preview wrap if no new icons are found
-          if (newIconsList.length === 0) {
-            document.querySelector('.latest').style.display = 'none'
-          } else {
-            document.querySelector('.latest').style.display = 'block'
-          }
-
-          const target = document.querySelector('.latest-icons')
-          // Filter EOS and MD icons
-          let newIcon
-          if (targetName === 'eos-icons') {
-            newIcon = newIconsList.filter((ele) => ele.tags.includes('eos'))
-          } else {
-            newIcon = newIconsList.filter((ele) => !ele.tags.includes('eos'))
-          }
-
-          // Appends each icon to the preview wrap
-          newIcon.forEach((ele) => {
-            const div = document.createElement('div')
-            // Detect the icon class in order to inject dinamycally
-            const iconClassQuery = document.querySelectorAll('i')
-            const iconClass = iconClassQuery[0].className
-
-            div.classList.add('icons__item')
-            div.setAttribute('name', ele.name)
-            div.innerHTML = `
-                  <i class="${iconClass}">
-                    ${ele.name}
-                  </i>
-                  <br>
-                  ${ele.name}`
-            target.append(div)
-          })
-        } catch (error) {
-          console.log(error)
+        // Removes the preview wrap if no new icons are found
+        if (newIconsList.length === 0) {
+          document.querySelector('.latest').style.display = 'none'
+        } else {
+          document.querySelector('.latest').style.display = 'block'
         }
+
+        const target = document.querySelector('.latest-icons')
+        // Filter EOS and MD icons
+        let newIcon
+        if (targetName === 'eos-icons') {
+          newIcon = newIconsList.filter((ele) => ele.tags.includes('eos'))
+        } else {
+          newIcon = newIconsList.filter((ele) => !ele.tags.includes('eos'))
+        }
+
+        // Appends each icon to the preview wrap
+        newIcon.forEach((ele) => {
+          const div = document.createElement('div')
+          // Detect the icon class in order to inject dinamycally
+          const iconClassQuery = document.querySelectorAll('i')
+          const iconClass = iconClassQuery[0].className
+
+          div.classList.add('icons__item')
+          div.setAttribute('name', ele.name)
+          div.innerHTML = `
+                <i class="${iconClass}">
+                  ${ele.name}
+                </i>
+                <br>
+                ${ele.name}`
+          target.append(div)
+        })
+      })
+      .catch((error) => {
+        console.error('Error:', error)
       })
   }
 })()
